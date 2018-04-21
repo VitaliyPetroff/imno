951058eeff3b3867a3401b8bf9a1d04e492f71a8
Vitaliy Petroff <petroff@mail.ru>
2018-04-21 14:02:35 +0300

    NEW:Load data from config

--- config.go
+++ config.go
@@ -1,5 +1,36 @@
 package main
 
-/*********************************************
-	This module works with config.
-*********************************************/
+import (
+	"github.com/BurntSushi/toml"
+)
+
+// Target - target host to check
+type Target struct {
+	Host []string `toml:"hosts"`
+}
+
+// Refresh - how often should made check availability
+type Refresh struct {
+	Wait int `toml:"delay"`
+}
+
+// Config - struct for all data in config
+type Config struct {
+	Target  Target  `toml:"target"`
+	Refresh Refresh `toml:"refresh"`
+}
+
+// NewConfig - create new Config instance
+func NewConfig() *Config {
+	return &Config{}
+}
+
+// Load - function load data from config
+func (c *Config) Load(configPath string) error {
+	_, err := toml.DecodeFile(configPath, c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import ()
+import (
+	"fmt"
+)
+
+// Panic - catch critical error
+func Panic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
+	// load config data
+	configPath := "config/imno.conf"
 
+	config := NewConfig()
+	err := config.Load(configPath)
+	Panic(err)
+
+	//print loaded config to stderr
+	fmt.Println(config)
 }
