786f51ddbf89f41b769190e418905dd38f0c26dd
Vitaliy Petroff <petroff@mail.ru>
2018-04-21 13:21:02 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # imno
 Project for monitoring own resources in war "rkn vs telegram".
+
+# Current version
+Version: In development!
---
+++ storage.go
@@ -0,0 +1,7 @@
+package main
+
+/*********************************************
+	This module works with storage.
+	To collect statistic and info from different
+	spyders into one storage.
+*********************************************/
